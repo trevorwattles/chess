50f50716e7383b43d58ded8b57cbe6e3a01df7bc
trevorwattles <twattles@byu.edu>
2025-02-27 00:31:57 -0700

    update main.java

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,16 @@
-import chess.*;
+import dataaccess.DataAccessException;
+import server.Server;
 
 public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+
+    public static void main(String[] args) throws DataAccessException {
+        /*try { DatabaseManager.createDatabase(); } catch (DataAccessException ex) {
+            throw new RuntimeException(ex);
+        }*/
+
+        System.out.println("♕ 240 Chess Server");
+        Server server = new Server();
+        int port = server.run(8080);
+
     }
 }
