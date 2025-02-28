6f12f329458c882eecdda3ac49693a3058a6b9bb
trevorwattles <twattles@byu.edu>
2025-02-28 00:06:42 -0700

    clear handler

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,26 @@
 package service;
 
+import dataaccess.MemoryUserDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.DataAccessException;
+
 public class ClearService {
+    private final MemoryUserDAO userDAO;
+    private final MemoryAuthDAO authDAO;
+    private final MemoryGameDAO gameDAO;
+
+    public ClearService() {
+        this.userDAO = new MemoryUserDAO();
+        this.authDAO = new MemoryAuthDAO();
+        this.gameDAO = new MemoryGameDAO();
+    }
+
+    public void clear() throws DataAccessException {
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+    }
 }
+
+
