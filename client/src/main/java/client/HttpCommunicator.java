bd1e83f1968385fc0e6cd2a3b215c15ff47303ff
trevorwattles <twattles@byu.edu>
2025-03-20 19:25:51 -0600

    created general outline for httpcommunicator and serverfacade

---
+++ client/src/main/java/client/HttpCommunicator.java
@@ -0,0 +1,44 @@
+package client;
+
+
+
+public class HttpCommunicator {
+
+
+    public HttpCommunicator() {
+
+    }
+
+    public register() {
+
+    }
+
+
+    public login() {
+
+    }
+
+    public logout() {
+
+    }
+
+    public createGame() {
+
+    }
+
+    public listGames() {
+
+    }
+
+    public  joinGame() {
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,43 @@
+package client;
+
+public class ServerFacade {
+
+
+    public ServerFacade() {
+
+    }
+
+    public  register() {
+
+    }
+
+    public login() {
+
+    }
+
+    public logout()  {
+
+    }
+
+    public listGames()  {
+
+    }
+
+    public createGame()  {
+
+    }
+
+    public joinGame() {
+    }
+
+
+    public  observeGame() {
+
+    }
+
+    public void clear()  {
+
+    }
+
+
+}
