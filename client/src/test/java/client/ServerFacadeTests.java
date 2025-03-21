c8ca5cbdccf74bf114f90228e65bb2a6007e99f6
trevorwattles <twattles@byu.edu>
2025-03-20 21:59:49 -0600

    passing both login and register tests for serverFacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -4,6 +4,7 @@ import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
 import server.Server;
 import server.request.RegisterRequest;
+import server.request.LoginRequest;
 
 
 public class ServerFacadeTests {
@@ -57,6 +58,42 @@ public class ServerFacadeTests {
             serverFacade.register(request);
         });
     }
+    @Test
+    public void testLogin_Success() throws ResponseException {
+        // Register first
+        serverFacade.register(new RegisterRequest("loginUser", "securePass123", "login@example.com"));
+
+        // Create LoginRequest and call login
+        LoginRequest loginRequest = new LoginRequest("loginUser", "securePass123");
+        var auth = serverFacade.login(loginRequest);
+
+        Assertions.assertNotNull(auth);
+        Assertions.assertNotNull(auth.authToken());
+    }
+
+    @Test
+    public void testLogin_WrongPassword() throws ResponseException {
+        serverFacade.register(new RegisterRequest("badLoginUser", "rightPassword", "badlogin@example.com"));
+
+        LoginRequest loginRequest = new LoginRequest("badLoginUser", "wrongPassword");
+
+        Assertions.assertThrows(ResponseException.class, () -> {
+            serverFacade.login(loginRequest);
+        });
+    }
+
+    @Test
+    public void testLogin_NonexistentUser() {
+        LoginRequest loginRequest = new LoginRequest("ghostUser", "doesntMatter");
+
+        Assertions.assertThrows(ResponseException.class, () -> {
+            serverFacade.login(loginRequest);
+        });
+    }
+
+
+
+
 
 }
 
