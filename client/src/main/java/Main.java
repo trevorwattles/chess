76a2621db6e4b2889d26ef9256273313eddf86ab
trevorwattles <twattles@byu.edu>
2025-03-22 17:00:54 -0600

    prelogin working in terminal (register, login, logout)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("♕ 240 Chess Client");
-
-        String serverUrl = "http://localhost:8080"; // Update this if your server runs on a different port
+        String serverUrl = "http://localhost:8080";
         ServerFacade serverFacade = new ServerFacade(serverUrl);
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("♕ 240 Chess Client");
         new BeforeLoginREPL(serverFacade, scanner).run();
     }
 }
