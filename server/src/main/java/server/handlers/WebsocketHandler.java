7a1eba9606175a0a33b2e490d7f04557e05fa679
trevorwattles <twattles@byu.edu>
2025-04-01 12:16:02 -0600

    set up websocketHandler

--- server/src/main/java/server/handlers/WebsocketHandler.java
+++ server/src/main/java/server/handlers/WebsocketHandler.java
@@ -1,4 +1,18 @@
 package server.handlers;
 
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
 public class WebsocketHandler {
+    @OnWebSocketConnect
+    public void onConnect(Session session) {}
+    @OnWebSocketClose
+    public void onClose(Session session, int statusCode, String reason) {}
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) {}
+
 }
